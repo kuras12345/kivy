f89ee6c9199e2b5371613484b4a5ec8873773f68
kuras12345 <74146850+kuras12345@users.noreply.github.com>
2026-04-07 17:15:05 +0200

    Pierwsza wersja aplikacji

---
+++ img/cookie.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+
+class Cookie(FloatLayout):
+   pass
+class CookieApp(App):
+       def build(self):
+        return Cookie()
+
+
+if __name__ == '__main__':
+    CookieApp().run()
